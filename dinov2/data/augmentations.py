2679261cc0c5fe08edf05617e2d06f44e74cf521
petrutiuvlad <37187104+petrutiuvlad@users.noreply.github.com>
2023-12-14 18:20:42 +0200

    v3

--- dinov2/data/augmentations.py
+++ dinov2/data/augmentations.py
@@ -6,6 +6,7 @@
 import logging
 
 from torchvision import transforms
+import torch
 
 from .transforms import (
     GaussianBlur,
@@ -15,6 +16,25 @@ from .transforms import (
 
 logger = logging.getLogger("dinov2")
 
+def gauss_noise_tensor(img, probability=0.25):
+    assert isinstance(img, torch.Tensor)
+    
+    # Check if Gaussian noise should be applied based on probability
+    if torch.rand(1).item() > probability:
+        return img
+    
+    dtype = img.dtype
+    if not img.is_floating_point():
+        img = img.to(torch.float32)
+    
+    sigma = 0.05
+    
+    out = img + sigma * torch.randn_like(img)
+    
+    if out.dtype != dtype:
+        out = out.to(dtype)
+        
+    return out
 
 class DataAugmentationDINO(object):
     def __init__(
@@ -60,10 +80,20 @@ class DataAugmentationDINO(object):
         )
 
         # color distorsions / blurring
-        color_jittering = transforms.Compose(
+        color_jittering_old = transforms.Compose(
             [
                 transforms.RandomApply(
                     [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
+                    p=0.8,
+                ),
+                transforms.RandomGrayscale(p=0.1),
+            ]
+        )
+        
+        color_jittering_new = transforms.Compose(
+            [
+                transforms.RandomApply(
+                    [transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.02)],
                     p=0.8,
                 ),
                 transforms.RandomGrayscale(p=0.2),
@@ -72,36 +102,62 @@ class DataAugmentationDINO(object):
 
         global_transfo1_extra = GaussianBlur(p=1.0)
 
-        global_transfo2_extra = transforms.Compose(
+        global_transfo2_extra_old = transforms.Compose(
             [
                 GaussianBlur(p=0.1),
                 transforms.RandomSolarize(threshold=128, p=0.2),
+            ]
+        )
+        
+        global_transfo2_extra_new = transforms.Compose(
+            [
+                GaussianBlur(p=0.1),
+                transforms.RandomSolarize(threshold=200, p=0.2),
             ]
         )
 
         local_transfo_extra = GaussianBlur(p=0.5)
 
         # normalization
-        self.normalize = transforms.Compose(
+        self.normalize_new = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                make_normalize_transform(),
+                gauss_noise_tensor,
+            ]
+        )
+        
+        self.normalize_old = transforms.Compose(
             [
                 transforms.ToTensor(),
                 make_normalize_transform(),
             ]
         )
 
-        self.global_transfo1 = transforms.Compose([color_jittering, global_transfo1_extra, self.normalize])
-        self.global_transfo2 = transforms.Compose([color_jittering, global_transfo2_extra, self.normalize])
-        self.local_transfo = transforms.Compose([color_jittering, local_transfo_extra, self.normalize])
-
+        self.global_transfo1_old = transforms.Compose([color_jittering_old, global_transfo1_extra, self.normalize_old])
+        self.global_transfo2_old = transforms.Compose([color_jittering_old, global_transfo2_extra_old, self.normalize_old])
+        self.local_transfo_old = transforms.Compose([color_jittering_old, local_transfo_extra, self.normalize_old])
+        
+        self.global_transfo1_new = transforms.Compose([color_jittering_new, global_transfo1_extra, self.normalize_new])
+        self.global_transfo2_new = transforms.Compose([color_jittering_new, global_transfo2_extra_new, self.normalize_new])
+        self.local_transfo_new = transforms.Compose([color_jittering_new, local_transfo_extra, self.normalize_new])
+        
     def __call__(self, image):
         output = {}
+        probability = 0.5
 
         # global crops:
         im1_base = self.geometric_augmentation_global(image)
-        global_crop_1 = self.global_transfo1(im1_base)
+        if torch.rand(1).item() < probability:
+            global_crop_1 = self.global_transfo1_old(im1_base)
+        else:
+            global_crop_1 = self.global_transfo1_new(im1_base)
 
         im2_base = self.geometric_augmentation_global(image)
-        global_crop_2 = self.global_transfo2(im2_base)
+        if torch.rand(1).item() < probability:
+            global_crop_2 = self.global_transfo2_old(im2_base)
+        else:
+            global_crop_2 = self.global_transfo2_new(im2_base)
 
         output["global_crops"] = [global_crop_1, global_crop_2]
 
@@ -109,9 +165,16 @@ class DataAugmentationDINO(object):
         output["global_crops_teacher"] = [global_crop_1, global_crop_2]
 
         # local crops:
-        local_crops = [
-            self.local_transfo(self.geometric_augmentation_local(image)) for _ in range(self.local_crops_number)
-        ]
+        local_crops = []
+        for _ in range(self.local_crops_number):
+            if torch.rand(1).item() < probability:
+                local_crop = self.local_transfo_old(self.geometric_augmentation_local(image))
+            else:
+                local_crop = self.local_transfo_new(self.geometric_augmentation_local(image))
+            local_crops.append(local_crop)
+        # local_crops = [
+        #     self.local_transfo(self.geometric_augmentation_local(image)) for _ in range(self.local_crops_number)
+        # ]
         output["local_crops"] = local_crops
         output["offsets"] = ()
 
